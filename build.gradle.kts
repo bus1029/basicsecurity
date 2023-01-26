8da6058781e62726d6feb7e26532ecdc3817f277
Seokhyun Bae <bus1029@gmail.com>
2023-01-26 14:35:32 +0900

    feat: SecurityConfig를 통한 사용자 정의 ID/Password 설정
    - WebSecurityConfigurerAdapter 클래스를 상속받은 
    SpringSecurity Configuration을 통해 사용자가 정의한 보안 설정을 
    사용하도록 수정
    --- Spring Security 5.7+ 부터 WebSecurityConfigurerAdapter 클래스가
    Deprecated 되었기 때문에 다른 방식으로 구현해야 함
    --- application.properties에 사용자 정의 ID/Password를 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,8 @@ repositories {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
-  implementation("org.springframework.book:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.security:spring-security-test")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
