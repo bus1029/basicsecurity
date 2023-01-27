3f37db72515aa5cc36631212d7e057c69e41ebd1
Seokhyun Bae <bus1029@gmail.com>
2023-01-27 11:36:48 +0900

    feat: FormLogin 인증 방식 설정
    - FormLogin 인증 방식에서 제공해주는 여러 옵션들을 사용하여
    각종 Handler 및 성공, 실패시 이동하는 URL 설정
    --- FormLogin에 사용되는 loginPage의 경우 인증하지 않고도 접근할
    수 있어야 하기 때문에 permitAll() 메소드 추가

--- src/main/kotlin/io/security/basicsecurity/SecurityConfig.kt
+++ src/main/kotlin/io/security/basicsecurity/SecurityConfig.kt
@@ -8,7 +8,11 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.Authentication
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnDefaultWebSecurity
@@ -19,12 +23,26 @@ class SecurityConfig {
   @Order(SecurityProperties.BASIC_AUTH_ORDER)
   fun filterChain(http: HttpSecurity): SecurityFilterChain {
     return http
-            .authorizeRequests()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .formLogin()
-            .and()
-            .build()
+      .authorizeRequests()
+      .anyRequest().authenticated()
+      .and()
+      .formLogin()
+//      .loginPage("/loginPage")
+      .defaultSuccessUrl("/")
+      .failureUrl("/login")
+      .usernameParameter("userId")
+      .passwordParameter("passwd")
+      .loginProcessingUrl("/login_proc")
+      .successHandler { _, response, authentication ->
+        println("authentication ${authentication.name}")
+        response.sendRedirect("/")
+      }
+      .failureHandler { _, response, exception ->
+        println("exception ${exception.message}")
+        response.sendRedirect("/login")
+      }
+      .permitAll()
+      .and()
+      .build()
   }
 }
--- src/main/kotlin/io/security/basicsecurity/SecurityController.kt
+++ src/main/kotlin/io/security/basicsecurity/SecurityController.kt
@@ -9,4 +9,9 @@ class SecurityController {
   fun index(): String {
     return "home"
   }
+
+  @GetMapping("loginPage")
+  fun loginPage(): String {
+    return "loginPage"
+  }
 }
