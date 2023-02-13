854cbe38791650d31fd238fc95834dc67ec34f41
Seokhyun Bae <bus1029@gmail.com>
2023-02-13 19:21:36 +0900

    feat: SecurityContext의 Authentication 비교 코드 추가
    - SecurityContext를 저장하는 SecurityContextHolder는 
    내부적으로 ThreadLocal을 사용하기 때문에, 같은 스레드라면 
    아무곳에서나 참조가 가능하도록 설계되었다.
    - 인증이 완료된 경우 HttpSession에도 SecurityContext가 저장되는데, 
    해당 SecurityContext의 Authentication과 SecurityContextHolder
    내부 SecurityContext의 Authentication은 같은 값을 가진다.
    - 그에 비해 새롭게 Thread를 생성해서 SecurityContext를 갖고 오려고
    할 경우, Thread 부모와 자식 간에는 SecurityContext가 공유가 안되기
    때문에 내부에 담긴 Authentication 또한 다르다.

--- src/main/kotlin/io/security/basicsecurity/SecurityController.kt
+++ src/main/kotlin/io/security/basicsecurity/SecurityController.kt
@@ -1,13 +1,36 @@
 package io.security.basicsecurity
 
+import org.springframework.security.core.context.SecurityContext
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpSession
 
 @RestController
 class SecurityController {
   @GetMapping("/")
-  fun index(): String {
+  fun index(httpSession: HttpSession): String {
+    val authentication = SecurityContextHolder.getContext().authentication
+    val securityContext =
+      httpSession.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY) as SecurityContext
+    if (authentication.equals(securityContext.authentication)) {
+      return "home true"
+    }
+
     return "home"
+  }
+
+  @GetMapping("/thread")
+  fun thread(): String {
+    val authentication = SecurityContextHolder.getContext().authentication
+    Thread {
+      val newAuthentication = SecurityContextHolder.getContext().authentication
+      if (!authentication.equals(newAuthentication)) {
+        println("Not equals")
+      }
+    }.start()
+    return "thread"
   }
 
   @GetMapping("loginPage")
